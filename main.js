c30af584f2d9937c3dc298165b42ec02eb3efec6
Htopik <49284279+Htopik@users.noreply.github.com>
2022-12-08 11:25:08 +0400

    Внесены следующие поправки: 1 Футер вынесен за раздел мейна 2 Соблюдена верная иерархия заголовков h1 h2 h3 3 Восстановлен шрифт текста внутри кнопки, а также она теперь работает 4 Произведена оптимизация части кода Проблема отступов решилась сама собой: заголовки контейнеров не были в 44 шрифте, из-за чего казалось, что пространства слишком много.

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 import 'normalize.css';
+const button = document.getElementById('buttonMore');
+const sectionSix = document.getElementById('section-sixth');
+button.addEventListener('click', () =>
+  sectionSix.scrollIntoView({behavior: 'smooth'})
+);
